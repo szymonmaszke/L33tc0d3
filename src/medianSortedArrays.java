882e9166089c5e75293e6f6920ca398815e65186
a395ux91 <vyz@protonmail.com>
2017-05-12 13:12:43 +0200

    O(n+m) solution
    Faster than 97% of inputs

--- src/medianSortedArrays.java
+++ src/medianSortedArrays.java
@@ -1,58 +1,27 @@
 import java.util.ArrayList;
 
-public class medianSortedArrays{
-  public double findMedianSortedArrays(int[] first, int[] second){
-    int mergedSize = first.length + second.length;
-    if(mergedSize % 2 == 0)
-      return evenMedian(first, second);
-    return oddMesian(first, second);
-
-  }
-
-  private double evenMedian(first, second){
-    int middle = (first.length+second.length)/2;
-    double median = 0;
-    int i=0, j=0;
-    while(i+j < middle){
-      if(first[i] > second[j])
-        median = first[i++];
-      else
-        median = second[j++];
+//O(n+m) solution, space O(n+m)
+public class Solution {
+    public double findMedianSortedArrays(int[] first, int[] second){
+        int[] mergedArray = mergeArrays(first, second);
+        //System.out.println((int)mergedArray.length/2);
+        //System.out.println(mergedArray[0] + " " + mergedArray[1] + " " + mergedArray[2]);
+        if(mergedArray.length % 2 == 0)
+            return ((double)mergedArray[mergedArray.length/2] + (double)mergedArray[mergedArray.length/2 - 1])/2.;
+        return (double)mergedArray[mergedArray.length/2];
     }
-    if(first[i] > second[j])
-      median += first[i];
-    else
-      median += second[j];
-
-    return median/2.;
-  }
-
-  private double oddMedian(first, second){
-    int middle = (first.length_second.length)/2;
-    double median = 0;
-    int i=0, j=0;
-    while(i+j < middle+2){
-      if(first[i] > second[j])
-        median = first[i++];
-      else
-        median = second[i++];
+    
+    private int[] mergeArrays(int[] first, int[] second){
+        int[] mergedArray = new int[first.length+second.length];
+        int i=0, j=0, k=0;
+        while(i<first.length && j<second.length){
+            if(first[i] < second[j])
+                mergedArray[k++] = first[i++];
+            else
+                mergedArray[k++] = second[j++];
+        }
+        System.arraycopy(first, i, mergedArray, k, (first.length -i));
+        System.arraycopy(second, j, mergedArray, k, (second.length -j));
+        return mergedArray;
     }
-    return median;
-  }
-
-  }
-  //use normal array
-  private int[] sortedArrayMerge(int[] first, int[] second){
-    int result[] = new int[first.length + second.length];
-    int i=0; j=0; k=0;
-    while(i<first.length && j<second.length){
-      if(first[i]>second[j])
-        result[k++] = first[i++];
-      else
-        result[k++] = second[j++];
-    }
-    System.arraycopy(first, i, result, k, first.length-i);
-    System.arraycopy(second, j, result, k, second.length-j);
-    return result;
-  }
 }
